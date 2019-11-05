ff3a106af0a3672f501007efc07702dd413b1126
Randi <rdnmax@gmail.com>
2019-11-06 02:05:27 +0530

    Add a method to load all customers

--- src/lk/ijse/dep/ManageCustomer/controller/UpdateCustomerController.java
+++ src/lk/ijse/dep/ManageCustomer/controller/UpdateCustomerController.java
@@ -1,5 +1,6 @@
 package lk.ijse.dep.ManageCustomer.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -7,11 +8,9 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import lk.ijse.dep.ManageCustomer.db.DBConnection;
+import lk.ijse.dep.ManageCustomer.util.CustomerTM;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UpdateCustomerController {
     public AnchorPane window1;
@@ -31,5 +30,18 @@ public class UpdateCustomerController {
     }
 
 
+    private void loadAllCustomers() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
 
+        ObservableList<CustomerTM> customers = tblCustome.getItems();
+        customers.clear();
+
+        while (rst.next()) {
+            customers.add(new CustomerTM(rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3)));
+        }
+    }
 }
