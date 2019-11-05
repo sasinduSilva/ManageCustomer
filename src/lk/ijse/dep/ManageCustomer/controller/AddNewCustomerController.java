04ecddfcccaf4efbb1df78c13aa120b9e6b1ccf9
Randi <rdnmax@gmail.com>
2019-11-05 22:44:20 +0530

    Add variables to AddNewCustomerController

--- src/lk/ijse/dep/ManageCustomer/controller/AddNewCustomerController.java
+++ src/lk/ijse/dep/ManageCustomer/controller/AddNewCustomerController.java
@@ -1,4 +1,25 @@
 package lk.ijse.dep.ManageCustomer.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AddNewCustomerController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TableView tblCustome;
+    public TableColumn clmId;
+    public TableColumn clmName;
+    public TableColumn clmAddress;
+    public Button btnNew;
+
+
+
+    public void addNewOnAction(ActionEvent actionEvent) {
+
+
+    }
 }
