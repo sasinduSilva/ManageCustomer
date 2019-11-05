8b36e5bf7d70fa95ef71047e18618dda8f6adf63
Randi <rdnmax@gmail.com>
2019-11-06 02:01:03 +0530

    Create UpdateCustomerController

---
+++ src/lk/ijse/dep/ManageCustomer/controller/UpdateCustomerController.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep.ManageCustomer.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class UpdateCustomerController {
+    public AnchorPane window1;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TableView tblCustome;
+    public TableColumn clmId;
+    public TableColumn clmName;
+    public TableColumn clmAddress;
+    public Button btnUpdate;
+
+
+
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+
+
+}
