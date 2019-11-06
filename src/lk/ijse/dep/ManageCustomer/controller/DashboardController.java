3915880958e3165a205edaafb6074b329f1f0f10
Sasindu <sinharaavishka@gmail.com>
2019-11-06 07:22:38 +0530

    add DeleteForm  navigations and SearchForm Navigations to dashboard

--- src/lk/ijse/dep/ManageCustomer/controller/DashboardController.java
+++ src/lk/ijse/dep/ManageCustomer/controller/DashboardController.java
@@ -1,8 +1,14 @@
 package lk.ijse.dep.ManageCustomer.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardController {
     public AnchorPane root;
@@ -17,9 +23,21 @@ public class DashboardController {
     public void btnUpdt_OnAction(ActionEvent actionEvent) {
     }
 
-    public void btnDlte_OnAction(ActionEvent actionEvent) {
+    public void btnDlte_OnAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/lk/ijse/dep/ManageCustomer/view/DeleteCustomer.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage1 = (Stage) this.root.getScene().getWindow();
+        stage1.setScene(scene);
+        stage1.centerOnScreen();
+        stage1.show();
     }
 
-    public void btnSrch_OnAction(ActionEvent actionEvent) {
+    public void btnSrch_OnAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/lk/ijse/dep/ManageCustomer/view/SearchForm.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage1 = (Stage) this.root.getScene().getWindow();
+        stage1.setScene(scene);
+        stage1.centerOnScreen();
+        stage1.show();
     }
 }
